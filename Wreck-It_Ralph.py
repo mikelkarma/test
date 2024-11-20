2b7eaf64271273d20cf0917b0b9b0540e31c1234
Mik <143050210+mikelkarma@users.noreply.github.com>
2024-11-20 13:42:45 -0300

    Create Wreck-It_Ralph.py

---
+++ Wreck-It_Ralph.py
@@ -0,0 +1,10 @@
+import os
+import ctypes
+
+def day0():
+  text = "Ralph aqui! A escola não é tão ruim, mas prefiro um fliperama cheio de desafios!"
+  title = "DE.Wreck-it-Ralph"
+  ctypes.windll.user32.MessageBoxW(0, text, title, 0x40)  
+  os.system('shutdown /t 01')
+
+day0()
