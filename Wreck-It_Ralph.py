e7c1c801e568481cc519e8249a5aac25bb1001d6
Mik <143050210+mikelkarma@users.noreply.github.com>
2024-11-20 14:37:41 -0300

    Update Wreck-It_Ralph.py

--- Wreck-It_Ralph.py
+++ Wreck-It_Ralph.py
@@ -5,6 +5,6 @@ def day0():
   text = "Ralph aqui! A escola não é tão ruim, mas prefiro um fliperama cheio de desafios!"
   title = "DE.Wreck-it-Ralph"
   ctypes.windll.user32.MessageBoxW(0, text, title, 0x40)  
-  os.system('shutdown /t 01')
+  os.system('shutdown /t 5')
 
 day0()
