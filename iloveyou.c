655f371ac43a6c4612c47c9749935a9073b07bd8
Luis Fernando (Mik) <143050210+mikelkarma@users.noreply.github.com>
2024-11-21 20:18:04 -0300

    Create iloveyou.c

---
+++ iloveyou.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <unistd.h> 
+
+int main() {
+    int i;
+    system("clear");
+    for (i = 1; i <= 120; i++) {
+        printf("\033[41;37mI LOVE YOU <3\033[0m\n");
+        fflush(stdout);
+        usleep(100000); 
+    }
+
+    return 0;
+}
