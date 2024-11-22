599f5f968f11890fcbd5c58906134ffdcecf14f5
Luis Fernando (Mik) <143050210+mikelkarma@users.noreply.github.com>
2024-11-22 18:19:39 -0300

    Create Hello_World.java

---
+++ Hello_World.java
@@ -0,0 +1,8 @@
+public class Hello_World {
+  public static void print(String text) {
+    System.out.println(text);
+  }
+  public static void main(String[] args) {
+    print("Hello World");
+  }
+}
