b80e591e60ea29875aa81276fc978b890ade5b36
Luis Fernando (Mik) <143050210+mikelkarma@users.noreply.github.com>
2024-11-23 19:26:50 -0300

    Update AES.java

--- AES.java
+++ AES.java
@@ -3,7 +3,7 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import java.util.Base64;
 
-public class Main {
+public class AES {
 
     // Gerando chave
     public static SecretKey generateAESKey() throws Exception {
