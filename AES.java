de274720327fae1610d77c2b1f445fb6e9cac2aa
Luis Fernando (Mik) <143050210+mikelkarma@users.noreply.github.com>
2024-11-23 19:21:27 -0300

    Update AES.java

--- AES.java
+++ AES.java
@@ -1,55 +1,56 @@
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
-public class AES {
-  // Gerando chave
-  public static SecretKey generateAESKey() throws Exception {
-    KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-    keyGenerator.init(128);
-    return keyGenerator.generateKey();
-  }
+public class Main {
 
-  // Encrypt AES
-  public static String encrypt(String data, SecretKey key) throws Exception {
-    Cipher cipher = Cipher.getInstance("AES");
-    cipher.init(Cipher.ENCRYPT_MODE, key);
-    byte[] decodedData = Base64.getDecoder().decode(data);
-    byte[] decryptedData = cipher.doFinal(decodedData);
-    return new String(decryptedData);
-  }
+    // Gerando chave
+    public static SecretKey generateAESKey() throws Exception {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128); 
+        return keyGenerator.generateKey(); 
+    }
 
-  // Decrypt AES
-  public static String decrypt(String data, SecretKey key) throws Exception {
+    // Encrypt AES
+    public static String encrypt(String data, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key); 
+        byte[] encryptedData = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedData);
+    
+    }
+
+
+    public static String decrypt(String data, SecretKey key) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.DECRYPT_MODE, key);
         byte[] decodedData = Base64.getDecoder().decode(data);
         byte[] decryptedData = cipher.doFinal(decodedData);
         return new String(decryptedData);
-  }
-  
-  // print :)
-  public static void print(String text) {
-    System.out.println(text);
-  }
-  
-  public static void main(String[] args) {
-    SecretKey key = generateAESKey(); // gerando key
+    }
 
-    // Criptogrando com  a key gerada
-    String data = "Informações confidencial";
-    print("> GenerateKEY: " + key);
-    print(" > Criptografando: " + data);
-    String crypt = encrypt(data + key);
-    print(" > Result: " + crypt);
+    // Método para printar
+    public static void print(String text) {
+        System.out.println(text);
+    }
 
-    // descriptografando
-    print("\n > Decrypting...");
-    String uncrypt = decrypt(crypt, key);
-    print(" > Result: " + uncrypt);
-    
-  }
-  
+    public static void main(String[] args) {
+        try {
+            SecretKey key = generateAESKey(); // Gerando chave
+            String data = "Informações confidenciais";
+            
+            print("> Gerando chave: " + key);
+            print(" > Criptografando: " + data);
+            String crypt = encrypt(data, key);
+            print(" > Resultado criptografado: " + crypt);
+
+            print("\n > Descriptografando...");
+            String uncrypt = decrypt(crypt, key); // Passando 'crypt' para descriptografar
+            print(" > Resultado descriptografado: " + uncrypt);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
